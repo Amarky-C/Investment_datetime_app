c698b1d5324ab279397e67e6854696e948ea9463
Uzoamaka Mbakogu <127051767+Amarky-C@users.noreply.github.com>
2023-05-18 13:32:28 +0100

    Cohort4_datetimeassignment

---
+++ investment_datetime_assign.py
@@ -0,0 +1,26 @@
+#Question 2 - Create an application that will give a 10% profit every 24hours
+
+#investment app that returns interest every 24hours
+#import time modules
+import time
+Profit_percentage = 10
+
+def investment_timer():
+    
+  #user enters name of the investor
+   name_of_investor = "Makason"
+   print('Welcome!', name_of_investor, 'to Cohort 4 Investment application')
+   
+   #user enters the amount to invest
+   wallet = 10000
+   
+   #application returns 10% profit on capital
+   interest_calc = wallet * Profit_percentage / 100
+   wallet_balance = wallet + interest_calc    
+   print(name_of_investor,'Your wallet has been credited with the sum of:','N', wallet_balance,"with a 10% interest on your capital")
+   
+    #add interest every 24hrs
+while True:
+   investment_timer()
+   time.sleep(24 * 60 * 60)  # 24 hours = 24 * 60 minutes * 60 seconds
+     
